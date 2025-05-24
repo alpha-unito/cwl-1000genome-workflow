533980bb882329b70619169d9234829131cfaae8
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-05-24 18:14:48 +0200

    Fix sifting remote location

--- download_data.sh
+++ download_data.sh
@@ -15,7 +15,7 @@ do
   wget -O "${SCRIPT_DIRECTORY}/data/20130502/ALL.chr${i}.250000.vcf.gz" \
     https://github.com/pegasus-isi/1000genome-workflow/raw/master/data/20130502/ALL.chr${i}.250000.vcf.gz
   wget -O "${SCRIPT_DIRECTORY}/data/20130502/sifting/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf.gz" \
-    ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/functional_annotation/filtered/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf.gz
+    https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/functional_annotation/filtered/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf.gz
 done
 
 for pop in "AFR" "ALL" "AMR" "EAS" "EUR" "GBR" "SAS"; do
